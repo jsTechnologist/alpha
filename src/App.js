1355b55f9ec4ede765f3891b37ef23e82c5eb18a
Ali rafie <aliarshi1812@gmail.comgit config --global user.email aliarshi1812@gmail.com>
2024-04-22 11:48:19 +0330

    add new file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+//import Job from "./components/Job";
+import { Class } from "./Class"; 
+function App(){
+
+  const classes = [
+   { name : "html" , finished : true },
+   { name : "css" , finished : true },
+   { name : "bootstrap" , finished : false},
+   { name : "javascript" , finished : false},
+   { name : "react" , finished : true},
+   { name : "pyton" , finished : false},
+   ] ;
+
+ 
+
+};
+export default App;
---
+++ src/Class.js
@@ -0,0 +1,10 @@
+ export  const Class =(props)=>{
+
+    
+    return (
+
+             <h2> {props.name} : {props.age} </h2>
+            
+    )
+};
+//export default User;
