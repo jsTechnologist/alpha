a6e28443f10bdc47ddbb62c39eef19569e345684
Ali rafie <aliarshi1812@gmail.comgit config --global user.email aliarshi1812@gmail.com>
2024-04-24 16:07:19 +0330

    change some file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
+import { Class } from "./Class";
 import "./App.css";
-import { Toturial } from "./Toturial";
-//import Job from "./components/Job";
-//import { Toturial } from "./Toturial"; 
+import { useState } from "react";
 function App(){
-
-  const toturiles = [
-   { name : "html" , finished : true },
-   { name : "css" , finished : true },
-   { name : "bootstrap" , finished : false},
-   { name : "javascript" , finished : false},
-   { name : "react" , finished : true},
-   { name : "pyton" , finished : false},
-   ] ;
-
+   
+  const [count , setCount]= useState(0);
+  
  return(
   <div className="App">
-       {toturiles.map((toturial,index)=>{
-        return toturial.finished && <h2>{toturial.name} </h2>
-       } ) }
-       <Toturial/>
+    <button  onClick={()=> setCount(count+1) }  > increase  </button> 
+    <button   onClick={()=> setCount(count-1) }> decrease  </button>
+    <button  onClick={()=> setCount( 0)}  > reset  </button>
+   <h1 > {count} </h1>
   </div>
  )
 
